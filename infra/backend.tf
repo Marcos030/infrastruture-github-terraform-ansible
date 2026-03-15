9b8261a1a4b93140c181e50ddea6827ab8ec48df
Marcos William <marcoswrsj@gmail.com>
2026-03-15 19:07:53 -0300

    Ajuste destroy

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,1 +1,9 @@
-# backend.tf (opcional)
+# infra/backend.tf
+terraform {
+  backend "s3" {
+    bucket         = "nome-do-seu-bucket-s3"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock" # Opcional, mas recomendado
+  }
+}
