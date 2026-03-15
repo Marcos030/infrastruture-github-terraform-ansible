8f1618b0d3d810abefab4b8b5d5661dbe0de8416
Marcos William <marcoswrsj@gmail.com>
2026-03-15 19:22:02 -0300

    ajuste formatacao

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,3 @@
-# main.tf
 provider "aws" {
   region = var.region
 }
@@ -6,7 +5,7 @@ provider "aws" {
 # 1. VPC e Network
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "vpc-automation" }
+  tags       = { Name = "vpc-automation-git-terraform-ansible" }
 }
 
 resource "aws_subnet" "subnet" {
@@ -32,50 +31,51 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.rt.id
 }
 
-# 2. Security Group (Equivalente ao NSG)
+# 2. Security Group
 resource "aws_security_group" "sg_automation" {
   name        = "sg_automation"
   description = "Allow SSH and App port"
   vpc_id      = aws_vpc.main.id
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Regras separadas (obrigatório no AWS Provider v5+)
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.sg_automation.id
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
 
-  ingress {
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "app" {
+  security_group_id = aws_security_group.sg_automation.id
+  from_port         = 8081
+  to_port           = 8081
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.sg_automation.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 # 3. Instância EC2
 resource "aws_instance" "vm" {
-  ami           = "ami-0c7217cdde317cfec" # Verifique se esta AMI Ubuntu é válida na sua região
-  instance_type = "t3.micro"            # t3.micro é geralmente a opção Free Tier atual
-  
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t3.micro"
+
   subnet_id                   = aws_subnet.subnet.id
   vpc_security_group_ids      = [aws_security_group.sg_automation.id]
   associate_public_ip_address = true
 
-  # CONFIGURAÇÃO PARA ANSIBLE COM SENHA (IGUAL AO LAB AZURE)
   user_data = <<-EOF
-              #!/bin/bash
-              echo "ubuntu:${var.admin_password}" | chpasswd
-              sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-              systemctl restart sshd
-              EOF
+    #!/bin/bash
+    echo "ubuntu:${var.admin_password}" | chpasswd
+    sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+    systemctl restart sshd
+  EOF
 
   tags = { Name = "vm-automation-git-terraform-ansible" }
 }
